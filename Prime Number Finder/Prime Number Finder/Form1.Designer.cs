9300d835f1491a5492e6666a6742e7d0cc994c95
Spaydo <1123jaky@gmail.com>
2018-10-15 16:37:38 +0800

    1.0.1

---
+++ Prime Number Finder/Prime Number Finder/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Prime_Number_Finder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtIn = new System.Windows.Forms.TextBox();
+            this.BtnFind = new System.Windows.Forms.Button();
+            this.TxtAnswer = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtSum = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TxtIn
+            // 
+            this.TxtIn.Font = new System.Drawing.Font("新細明體", 12F);
+            this.TxtIn.Location = new System.Drawing.Point(12, 28);
+            this.TxtIn.Name = "TxtIn";
+            this.TxtIn.Size = new System.Drawing.Size(144, 27);
+            this.TxtIn.TabIndex = 0;
+            // 
+            // BtnFind
+            // 
+            this.BtnFind.Font = new System.Drawing.Font("新細明體", 12F);
+            this.BtnFind.Location = new System.Drawing.Point(162, 26);
+            this.BtnFind.Name = "BtnFind";
+            this.BtnFind.Size = new System.Drawing.Size(54, 27);
+            this.BtnFind.TabIndex = 1;
+            this.BtnFind.Text = "查詢";
+            this.BtnFind.UseVisualStyleBackColor = true;
+            this.BtnFind.Click += new System.EventHandler(this.BtnFind_Click);
+            // 
+            // TxtAnswer
+            // 
+            this.TxtAnswer.Enabled = false;
+            this.TxtAnswer.Font = new System.Drawing.Font("新細明體", 12F);
+            this.TxtAnswer.Location = new System.Drawing.Point(12, 77);
+            this.TxtAnswer.Multiline = true;
+            this.TxtAnswer.Name = "TxtAnswer";
+            this.TxtAnswer.ReadOnly = true;
+            this.TxtAnswer.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TxtAnswer.Size = new System.Drawing.Size(400, 300);
+            this.TxtAnswer.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(442, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "下方輸入 二 ~ 四十億 整數(依電腦性能建議最高不超過一千萬)";
+            // 
+            // TxtSum
+            // 
+            this.TxtSum.Font = new System.Drawing.Font("新細明體", 12F);
+            this.TxtSum.Location = new System.Drawing.Point(376, 28);
+            this.TxtSum.Name = "TxtSum";
+            this.TxtSum.ReadOnly = true;
+            this.TxtSum.Size = new System.Drawing.Size(100, 27);
+            this.TxtSum.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label2.Location = new System.Drawing.Point(222, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "範圍內質數數量為 : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 12F);
+            this.label3.Location = new System.Drawing.Point(12, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(354, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "下列顯示找到的質數(由於耗效能此功能暫時關閉)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 57);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtSum);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TxtAnswer);
+            this.Controls.Add(this.BtnFind);
+            this.Controls.Add(this.TxtIn);
+            this.Name = "Form1";
+            this.Text = "質數查詢器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtIn;
+        private System.Windows.Forms.Button BtnFind;
+        private System.Windows.Forms.TextBox TxtAnswer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtSum;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Prime Number Finder/Prime Number Finder/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+#region 質數特性
+  /*
+  1.最小的質數是2。
+  2.除2以外的質數都是奇數。
+  3.所有非質數都可做質因數分解。
+  4.非質數的最大質因數不會大於他的平方根。
+  */
+#endregion
+
+namespace Prime_Number_Finder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TxtIn.Focus();
+        }
+        #region 變數
+        bool isError;//用作判斷是否程式錯誤
+        uint number;//儲存使用者輸入值
+        uint maxValue;//儲存被判斷數值的平方根
+        int sum;//儲存質數數量
+        uint prime;
+        #endregion
+        private void BtnFind_Click(object sender, EventArgs e)
+        {
+            SetDefault();//設定預設值
+            SetNumberToInput();//指定 number 為使用者輸入值
+            if (isError) return;//輸入值錯誤就不做後續判斷
+            if (number < 2)
+            {
+                TxtAnswer.Text = "請輸入2或2以上整數";
+                return;
+            }
+            PrimeJudgment();//判斷哪個數值是質數
+            TxtSum.Text = sum.ToString();
+        }
+        void SetDefault()//設定預設值
+        {
+            TxtAnswer.Text = "";
+            //TxtAnswer.Text = "2";
+            isError = false;
+            sum = 1;
+        }
+        void SetNumberToInput()//指定 number 為使用者輸入值
+        {
+            try
+            {
+                number = Convert.ToUInt32(TxtIn.Text);
+            }
+            catch (FormatException ex)
+            {
+                TxtAnswer.Text = "格式轉換錯誤，請輸入整數數值資料";
+                isError = true;
+            }
+            catch (OverflowException ex)
+            {
+                TxtAnswer.Text = "數值超出範圍，請輸入 0 ~ 4,294,967,295 之間整數值";
+                isError = true;
+            }
+            catch (Exception ex)
+            {
+                TxtAnswer.Text = ex.Message; //這句輸出簡單的錯誤說明
+                //TxtAnswer.Text = ex.ToString(); //這句輸出詳細的錯誤說明
+                isError = true;
+            }
+        }
+        void PrimeJudgment()//判斷哪個數值是質數
+        {
+            for (prime = 1; prime < number; prime += 2)
+            {
+                maxValue = (uint)Math.Sqrt(prime);
+                int h = 1;
+                while (h < prime)
+                {
+                    h++;
+                    if (prime % h == 0)
+                    {
+                        break;
+                    }
+                    if (h > maxValue)
+                    {
+                        sum++;
+                        //SavePrime();//儲存質數
+                        //PrintPrime();//顯示質數列表
+                        break;
+                    }
+                }
+            }
+        }
+        void SavePrime()//儲存質數
+        {
+            
+        }
+        void PrintPrime()//顯示質數列表
+        {
+            TxtAnswer.Text += "、" + prime;
+        }
+    }
+}
